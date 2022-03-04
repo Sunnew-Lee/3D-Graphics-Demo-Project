58cb632b7e33491f4f991bc68a8397d17abedf20
donga-DigiPen <65002085+donga-DigiPen@users.noreply.github.com>
2022-03-04 22:42:12 +0900

    change color

--- jjamptong/tutorial-1/glnew.cpp
+++ jjamptong/tutorial-1/glnew.cpp
@@ -31,7 +31,7 @@ const char* GLNew::fragmentShaderSource =
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\n\0"; ;
 
 
@@ -129,7 +129,7 @@ void GLNew::draw()
     {
         processInput(window);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
